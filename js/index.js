69c105efa6ddfd85fb50c11f8e3397242744c279
vishal1605 <vishalgt581@gmail.com>
2022-01-23 09:30:33 +0530

    Js tutorial started

---
+++ js/index.js
@@ -0,0 +1,23 @@
+/*document.write("hello World<br/>");
+document.write("how are you");*/
+console.log("how u doing");
+var a = "vishal gupta";
+var b = 9004;
+a=1042;   // You can see here we change string value to int and its working
+document.write(a+"<br/>"+b);
+
+/*Write way to take variable in JSON
+firstname
+first_name
+first-name
+firstname
+firstName99*/
+
+/*Wrong way to take variables
+first name
+99firstname*/
+
+var p, q, r;
+
+p=2;
+document.write("<br/>"+p);
---
+++ js/menu.js
@@ -0,0 +1,12 @@
+var menuBar = document.getElementById("bar");
+var menu = document.getElementById("menu1");
+
+function show(){
+    if(menu.style.display=="block"){
+        menu.style.display="none";
+
+    }else{
+        menu.style.display="block";
+
+    }
+}
